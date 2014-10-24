91e89c16fa3dd07e4e535b0f774ec08043b4ab28
Mao-xy <15850563830@139.com>
2014-10-24 15:06:38 +0800

    验证

--- js/change.js
+++ js/change.js
@@ -3,9 +3,14 @@ function change(){
 	var content2 = document.getElementById("status").innerHTMl;
 	var lastTime = document.getElementById("time").value; 
 	if(content1=="RUN"){ 
-	   document.getElementById("run").value = "STOP";
-	   document.getElementById("status").innerHTMl = "Running"; 
-	   var t = setTimeout("stop();",lastTime);
+	    if(document.getElementById("time").value&&document.getElementById("total").value){
+	       document.getElementById("run").value = "STOP";
+	       document.getElementById("status").innerHTMl = "Running"; 
+	       var t = setTimeout("stop();",lastTime);
+	    }
+		else{
+		   alert("到场人数和限定时间不可为空。");
+		}
 	}
     else{
 	   document.getElementById("run").value = "RUN";
