3528fe2250e708b738ab4e2d8c3570868eca3844
Mao-xy <15850563830@139.com>
2014-10-24 14:59:09 +0800

    点点点

--- js/change.js
+++ js/change.js
@@ -1,19 +1,19 @@
 function change(){
-    var content1 = document.getElementById("run").value;
+    var content1 = document.getElementById("run").value; 
 	var content2 = document.getElementById("status").innerHTMl;
-	var lastTime = document.getElementById("time").value;
-	if(content=="RUN"){
-	   documnet.getElementById("run").value = "Stop";
-	   document.getElementById("status").innerHTMl = "Running";
-	   var t = setTimeOut("stop();",lastTime);
+	var lastTime = document.getElementById("time").value; 
+	if(content1=="RUN"){ 
+	   document.getElementById("run").value = "STOP";
+	   document.getElementById("status").innerHTMl = "Running"; 
+	   var t = setTimeout("stop();",lastTime);
 	}
     else{
-	   documnet.getElementById("run").value = "Run";
+	   document.getElementById("run").value = "RUN";
 	   document.getElementById("status").innerHTMl = "Stopped";
 	}
 }
 
-function stop(){
-    documnet.getElementById("run").value = "Run";
+function stop(){ 
+    document.getElementById("run").value = "RUN";
 	document.getElementById("status").innerHTMl = "Stopped";
 }
