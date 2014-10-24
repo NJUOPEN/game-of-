59ed8ea514d866383da614f4ea54797430cf68e0
Mao-xy <15850563830@139.com>
2014-10-24 14:31:36 +0800

    计时器

--- js/change.js
+++ js/change.js
@@ -1,12 +1,19 @@
 function change(){
     var content1 = document.getElementById("run").value;
-	var content2 = document.getElementById("status").innerHTMl;;
+	var content2 = document.getElementById("status").innerHTMl;
+	var lastTime = document.getElementById("time").value;
 	if(content=="RUN"){
 	   documnet.getElementById("run").value = "Stop";
 	   document.getElementById("status").innerHTMl = "Running";
+	   var t = setTimeOut("stop();",lastTime);
 	}
     else{
 	   documnet.getElementById("run").value = "Run";
 	   document.getElementById("status").innerHTMl = "Stopped";
 	}
+}
+
+function stop(){
+    documnet.getElementById("run").value = "Run";
+	document.getElementById("status").innerHTMl = "Stopped";
 }
